dfd1ca7032fe3c0dec8511822205d1a13ae574e1
amirulsyafaat <amirulsyafaat90@gmail.com>
2026-09-15 15:41:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -511,9 +511,3 @@ if __name__ == '__main__':
     ensure_order_columns()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
